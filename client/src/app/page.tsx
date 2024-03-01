cdde9bfe57c6643d1f3886fa81b84648aec9cbc7
Dima <dmytro.stakhov@gmail.com>
2024-03-01 12:33:46 +0200

    fixed Tailwind compiling

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,9 +1,17 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-
-    </main>
+      <div className={"flex min-h-screen flex-col justify-between"}>
+          <header className={"w-full"}>header</header>
+          <main className="w-full flex-grow">
+              <span className={"heading-1 text-highlight"}>xalupa</span>
+              <span className={"heading-2 dima"}>test 2</span>
+              <span className={"heading-3 text-testtest"}>  test 3</span>
+              <span className={"heading-4"}>test 4</span>
+              <span className={"heading-5"}>test 5</span>
+          </main>
+          <footer className={"w-full"}>
+            footer
+          </footer>
+      </div>
   );
 }
--- client/tailwind.config.ts
+++ client/tailwind.config.ts
@@ -1,5 +1,7 @@
 import type { Config } from "tailwindcss";
 
+const plugin = require('tailwindcss/plugin')
+
 const config: Config = {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
@@ -8,13 +10,34 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
+      backgroundImage: {},
+      colors: {
+        'highlight': '#D4B254',
+        'title-text': '#222222',
+        'menu-icons': '#000000',
+        'light': '#eeeeee',
+        'body-text': '#777777',
+        'success': '#86CB19',
+        'success-text': '#75B118',
+        'danger': '#FF0000',
+        'danger-bg': '#FFF5F5',
+        'danger-text': '#CC0000',
+        'warning': '#FFC928',
+        'warning-bg': '#FFC9281A',
+        'warning-text': '#9F7F1E',
+      }
     },
   },
-  plugins: [],
+  plugins: [
+    plugin(function ({ addComponents, theme }) {
+      addComponents({
+        '.heading-1': {
+          'fontWeight': '600',
+          'fontSize': '52px',
+          // lineHeight: 'calc(58 / 52 * 1em)',
+        }
+      })
+    })
+  ],
 };
 export default config;
